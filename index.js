4a87dd66c2327940665c86a1e279970e23178e59
ahanafislam <ahanafislam@gmail.com>
2022-05-28 07:20:51 +0600

    Genarate access token and add user to database

--- index.js
+++ index.js
@@ -13,12 +13,28 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qhpbi.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: 'UnAuthorized' });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden Access' })
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
 async function run() {
   try {
     await client.connect();
     const productsCollection = client.db("fixtool").collection("products");
     const reviewCollection = client.db("fixtool").collection("review");
     const orderCollection = client.db("fixtool").collection("order");
+    const usersCollection = client.db("fixtool").collection("users");
 
     /*==========================
         Endpoints For Products
@@ -111,6 +127,24 @@ async function run() {
       const review = req.body;
       const result = await reviewCollection.insertOne(review);
       res.send(result);
+    });
+
+    /*=====================================
+        Endpoints For All Types of user
+    ======================================*/
+
+    // Upsert user and genarate token
+    app.put('/user/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = {email: email};
+      const options = {upsert: true};
+      const updateDoc = {
+        $set: user,
+      }
+      const result = await usersCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
+      res.send({ result, token });
     });
 
   }
